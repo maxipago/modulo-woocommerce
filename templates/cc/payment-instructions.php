06f88845c8e2bf04af467dd2283d0aa2f6858fd6
Danilo Correa de Benedetti <danilo.bene@uol.com.br>
2017-10-25 18:30:45 -0200

    maxiPago-woocommerce
    Ajuste no envio do CPF para vendas via boleto

--- templates/cc/payment-instructions.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!defined('ABSPATH')) {
-    exit;
-}
-?>
-<div class="woocommerce-message">
-    <span><?php printf(__('Payment successfully: %s in %s.', 'woocommerce-maxipago'), '<strong>' . $brand . '</strong>', '<strong>' . $installments . 'x</strong>'); ?></span>
-</div>
--- templates/ticket/payment-instructions.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
-?>
-<div class="woocommerce-message">
-	<span><a class="button" href="<?php echo esc_url( $url ); ?>" target="_blank" style="display: block !important; visibility: visible !important;"><?php _e( 'Pay the Order', 'woocommerce-maxipago' ); ?></a><?php _e( 'Please click in the following button to view your Ticket.', 'woocommerce-maxipago' ); ?><br /><?php _e( 'You can print and pay in your internet banking or in a lottery retailer.', 'woocommerce-maxipago' ); ?><br /><?php _e( 'After we receive the payment confirmation, your order will be processed.', 'woocommerce-maxipago' ); ?></span>
-</div>
